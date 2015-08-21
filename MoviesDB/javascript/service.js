f3ec25cfe109facb26ea92505d67e2df9134b2f3
Sean Cahill <projects@nmm.ie>
2015-08-21 15:38:47 +0100

    First Commit

---
+++ MoviesDB/javascript/service.js
@@ -0,0 +1,13 @@
+angular.module('movieDBServices',[]).factory('MovieListService',function($http) {
+//  
+  
+    return  {
+            getList: getList
+        };
+
+     
+    function getList(url){
+        return $http.get(url);
+        };
+    
+  });
